b9645a38918883f1d28a27f45c29c3ed9cfc6da4
Kaylan Souza <kaylansouza2@gmai.com>
2023-10-09 15:47:06 -0300

    Deixando ainda mais responsivo

--- index.js
+++ index.js
@@ -18,3 +18,19 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 )}
 scrollSuave()
+
+const menuBars = document.getElementById("menu-bars");
+const navigation = document.querySelector(".navegação1 ul");
+const menuIcon = document.querySelector(".menu-icon");
+
+menuBars.addEventListener("click", () => {
+  navigation.classList.toggle("active");
+  menuIcon.classList.toggle("active"); // Adicione esta linha
+
+  // Verifique se a classe "active" está presente no ícone e atualize seu conteúdo
+  if (menuIcon.classList.contains("active")) {
+    menuIcon.innerHTML = "&#10005;"; // Código HTML para o ícone "X"
+  } else {
+    menuIcon.innerHTML = "&#9776;"; // Código HTML para o ícone do menu
+  }
+});
